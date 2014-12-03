e63fc688730f4c39ff8e9d77e413bd3158814227
Sergey Mironov <smironov@ecotelecom.ru>
2014-12-03 13:34:41 +0100

    scb8970.nix: add timezone

--- src/scb8970.nix
+++ src/scb8970.nix
@@ -1,21 +1,25 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       /etc/nixos/hardware-configuration.nix
+      ./include/devenv.nix
+      ./include/screenrc.nix
+      ./include/bashrc.nix
+      ./include/systools.nix
+      ./include/security.nix
+      ./include/templatecfg.nix
+      ./include/workusers.nix
+      <nixos/modules/programs/virtualbox.nix>
     ];
 
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda";
   boot.kernelParams = ["console=ttyS0,115200"];
+
+  time.timeZone = "Europe/Moscow";
 
   networking.hostName = "scb8970";
   networking.useDHCP = false;
@@ -34,44 +38,17 @@
   services.openssh.ports = [22 2222];
   services.openssh.permitRootLogin = "yes";
 
+  services.ntp.enable = true;
+  services.ntp.servers = [ "0.pool.ntp.org" "1.pool.ntp.org" "2.pool.ntp.org" ];
 
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "lat9w-16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
-  # List packages installed in system profile. To search by name, run:
-  # -env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     wget
     git
     vim
   ];
 
-  # List services that you want to enable:
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.kdm.enable = true;
-  # services.xserver.desktopManager.kde4.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.extraUsers.guest = {
-  #   name = "guest";
-  #   group = "users";
-  #   uid = 1000;
-  #   createHome = true;
-  #   home = "/home/guest";
-  #   shell = "/run/current-system/sw/bin/bash";
-  # };
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
 
 }
