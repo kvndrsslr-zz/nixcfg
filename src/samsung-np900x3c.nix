5d3dd26db402d84c7dddd051619a799c83e4962b
Sergey Mironov <grrwlf@gmail.com>
2014-12-05 20:18:21 +0300

    np900x3c.nix: more xfce overrides

--- src/samsung-np900x3c.nix
+++ src/samsung-np900x3c.nix
@@ -195,7 +195,24 @@ rec {
       jre = true;
       enableAdobeFlash = true;
     };
-  };
 
+    packageOverrides = pkgs : {
+      xfce = pkgs.xfce // {
+
+        xfce4_xkb_plugin = pkgs.lib.overrideDerivation (pkgs.xfce.xfce4_xkb_plugin) (o:{
+          name = o.name + "-patched";
+          patches = [ ./pkgs/xfce4-xkb.patch ];
+        });
+
+        thunar = pkgs.lib.overrideDerivation pkgs.xfce.thunar (a:{
+          name = a.name + "-patched";
+          prePatch = ''
+            cp -pv ${pkgs.callPackage ./pkgs/thunar_uca.nix {}} plugins/thunar-uca/uca.xml.in
+          '';
+        });
+      };
+    };
+
+  };
 }
 
