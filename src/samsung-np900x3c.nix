8c29d1a2cfadf2a201f93686e10f4524002b2a19
Sergey Mironov <grrwlf@gmail.com>
2015-04-04 12:34:43 +0300

    xfce: move xfce packages to the common include

--- src/samsung-np900x3c.nix
+++ src/samsung-np900x3c.nix
@@ -163,19 +163,6 @@ rec {
     xlsfonts
     djvulibre
     wine
-    xfce.xfce4_cpufreq_plugin
-    xfce.xfce4_systemload_plugin
-    xfce.xfce4_xkb_plugin
-    xfce.xfce4_embed_plugin
-    xfce.xfce4_battery_plugin
-    xfce.xfce4_verve_plugin
-    xfce.xfce4_notes_plugin
-    xfce.xfce4_dict_plugin
-    xfce.xfce4_clipman_plugin
-    xfce.xfce4_datetime_plugin
-    xfce.xfce4_netload_plugin
-    xfce.gigolo
-    xfce.xfce4taskmanager
     vlc
     libreoffice
     pidgin
