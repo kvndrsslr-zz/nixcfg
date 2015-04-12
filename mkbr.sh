99efd82459e47c28609c243f68bf4f942dd6c8f6
Sergey Mironov <grrwlf@gmail.com>
2015-04-12 21:18:33 +0300

    mkbr.sh: add the script

---
+++ mkbr.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+ME=mkbr.sh
+die() { echo $ME: $@ >&2 ; exit 1; }
+
+echo $@ | grep -qwE -e '-h|--help' && {
+  echo "$ME <commits> - Cherry pick the commits specified to a new branch derived directly from the master" >&2
+  return 1
+}
+
+set -e
+
+if test -z "$@" ; then
+  cmts=`git rev-parse HEAD`
+else
+  cmts=`git rev-parse "$@"`
+fi
+
+cwd=`pwd`
+cd $NIX_DEV_ROOT/nixpkgs
+
+echo -n "Enter name for the git branch: "
+read bname
+
+(
+set -x
+cb=`git branch --list | grep '*' | awk '{print $2}'`
+base=`git merge-base "$cb" origin/master`
+{ git branch $bname $base || echo "$ME: branch $bname exists? Well, OK" ; }
+git checkout "$bname"
+git cherry-pick $cmts
+git checkout "$cb"
+) >&2
+
+if [ "$?" != "0" ] ; then {
+  echo "$ME: Cherry-picking failed. Do some git magic to fix it."
+  } >&2
+  return 1
+fi
+
+cd $cwd
