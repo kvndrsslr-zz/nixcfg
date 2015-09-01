3b71ad09edd92b0e005b3150a8d34a8cc45dcf6c
Sergey Mironov <grrwlf@gmail.com>
2015-09-01 11:33:27 +0300

    stunnel: client reads proxy settings

--- ideas/stunnel-test/client.sh
+++ ideas/stunnel-test/client.sh
@@ -2,6 +2,15 @@
 
 CWD=`pwd`
 
+parse() {
+  U=`echo $1 | tr ':@' '  ' | awk '{print $1}'`
+  P=`echo $1 | tr ':@' '  ' | awk '{print $2}'`
+  H=`echo $1 | tr ':@' '  ' | awk '{print $3}'`
+  Po=`echo $1 | tr ':@' '  ' | awk '{print $4}'`
+}
+
+parse $http_proxy
+
 cat >cfg.client <<EOF
 cert=$CWD/stunnel.pem
 pid=$CWD/stunnel.pid
@@ -10,9 +19,15 @@ client=yes
 # setgid = stunnel
 foreground = yes
 output = /dev/stdout
+debug = 7
 [ssh]
+protocol = connect
+protocolHost = 46.38.250.132:443
+protocolUsername = $U
+protocolPassword = $P
 accept=443
-connect=46.38.250.132:443
+connect=$H:$Po
 EOF
 
 stunnel cfg.client
+
