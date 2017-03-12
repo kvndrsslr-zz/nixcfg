5e1be20fc14583d72de6d09caa7bced177007415
betaboon <betaboon@0x80.ninja>
2017-03-12 23:15:34 +0000

    vps -> containers: add test-container (nginx)

--- src/vps-netcup.nix
+++ src/vps-netcup.nix
@@ -110,6 +110,30 @@ rec {
     '';
   };
 
+  containers.test = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.100.100";
+    localAddress = "192.168.100.101";
+
+    config = { config, pkgs, ...}: {
+      networking.firewall = {
+        enable = true;
+        allowedTCPPorts = [ 80 ];
+      };
+      services.nginx = {
+        enable = true;
+        httpConfig = ''
+          server {
+            listen 80;
+            server_name test.gargantua1.0x80.ninja;
+            root /var/www/;
+          }
+        '';
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs ; [
     vim
     tig
