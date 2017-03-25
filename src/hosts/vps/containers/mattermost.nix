0a371b4c4a98ba9ffccedd4024c81848a31344e4
betaboon <betaboon@0x80.ninja>
2017-03-25 23:25:22 +0000

    vps->containers->mattermost: setup is running

--- src/hosts/vps/containers/mattermost.nix
+++ src/hosts/vps/containers/mattermost.nix
@@ -11,25 +11,26 @@ in{
     hostAddress = "192.168.101.100";
     localAddress = "192.168.101.101";
 
-    bindMounts = {
-      "/webroot" = { 
-        hostPath = "/var/www/static-page";
-        isReadOnly = true; 
-        };
-    };
-
     config = { config, pkgs, ...}: {
       networking.enableIPv6 = false;
       networking.firewall = {
         enable = true;
-        allowedTCPPorts = [ 80 ];
+        allowedTCPPorts = [
+          8065 # mattermost
+        ];
       };
-      services.nginx = {
+      services.postgresql = {
         enable = true;
-        virtualHosts."${hostname}" = {
-          enableSSL = false;
-          root = "/webroot";
-        };
+        authentication = pkgs.lib.mkForce ''
+          local  all all trust
+          local  all all trust
+          host  all all 127.0.0.1/32 trust
+          host  all all ::1/128      trust
+        '';
+      };
+      services.mattermost = {
+        enable = true;
+        siteUrl = "https://mattermost.gargantua1.0x80.ninja"; #TODO this should be more generic
       };
     };
   };
--- src/hosts/vps/services/nginx.nix
+++ src/hosts/vps/services/nginx.nix
@@ -27,7 +27,40 @@
         forceSSL = true;
         enableACME = true;
         locations."/" = {
-          proxyPass = "http://192.168.101.101";
+          proxyPass = "http://192.168.101.101:8065";
+          extraConfig = ''
+            client_max_body_size 50M;
+            proxy_set_header Connection "";
+            proxy_set_header Host $http_host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Frame-Options SAMEORIGIN;
+            proxy_buffers 256 16k;
+            proxy_buffer_size 16k;
+            proxy_read_timeout 600s;
+            # proxy_cache mattermost_cache;
+            # proxy_cache_revalidate on;
+            # proxy_cache_min_uses 2;
+            # proxy_cache_use_stale timeout;
+            # proxy_cache_lock on;
+          '';
+        };
+        locations."/api/v3/users/websocket" = {
+          proxyPass = "http://192.168.101.101:8065";
+          extraConfig = ''
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+            client_max_body_size 50M;
+            proxy_set_header Host $http_host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Frame-Options SAMEORIGIN;
+            proxy_buffers 256 16k;
+            proxy_buffer_size 16k;
+            proxy_read_timeout 600s;
+          '';
         };
       };
     };
