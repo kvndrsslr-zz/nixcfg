8440edde3b0e57db1fca24eecf4d3be853fec584
betaboon <betaboon@0x80.ninja>
2017-03-26 18:41:28 +0000

    vps->containers(mattermost): adapt reverse-proxy settings

--- src/hosts/vps/containers/mattermost.nix
+++ src/hosts/vps/containers/mattermost.nix
@@ -1,15 +1,60 @@
 { config, lib, pkgs, ... }:
 let
 
-  hostname = "mattermost";
+  hostName = "mattermost";
+  domainName = "gargantua1.0x80.ninja";
+  fqdn = "${hostName}.${domainName}";
+
+  hostAddress = "192.168.101.100";
+  localAddress = "192.168.101.101";
 
 in{
+  services.nginx.virtualHosts."${fqdn}" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://${localAddress}:8065";
+      extraConfig = ''
+        client_max_body_size 50M;
+        proxy_set_header Connection "";
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Frame-Options SAMEORIGIN;
+        proxy_buffers 256 16k;
+        proxy_buffer_size 16k;
+        proxy_read_timeout 600s;
+        # proxy_cache mattermost_cache;
+        # proxy_cache_revalidate on;
+        # proxy_cache_min_uses 2;
+        # proxy_cache_use_stale timeout;
+        # proxy_cache_lock on;
+      '';
+    };
+    locations."/api/v3/users/websocket" = {
+      proxyPass = "http://${localAddress}:8065";
+      extraConfig = ''
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        client_max_body_size 50M;
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Frame-Options SAMEORIGIN;
+        proxy_buffers 256 16k;
+        proxy_buffer_size 16k;
+        proxy_read_timeout 600s;
+      '';
+    };
+  };
 
-  containers."${hostname}" = {
+  containers."${fqdn}" = {
     autoStart = true;
     privateNetwork = true;
-    hostAddress = "192.168.101.100";
-    localAddress = "192.168.101.101";
+    hostAddress = "${hostAddress}";
+    localAddress = ${localAddress}";
 
     config = { config, pkgs, ...}: {
       networking.enableIPv6 = false;
@@ -22,15 +67,13 @@ in{
       services.postgresql = {
         enable = true;
         authentication = pkgs.lib.mkForce ''
-          local  all all trust
-          local  all all trust
           host  all all 127.0.0.1/32 trust
           host  all all ::1/128      trust
         '';
       };
       services.mattermost = {
         enable = true;
-        siteUrl = "https://mattermost.gargantua1.0x80.ninja"; #TODO this should be more generic
+        siteUrl = "https://${fqdn}"; #TODO this should be more generic
       };
     };
   };
