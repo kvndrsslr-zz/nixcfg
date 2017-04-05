3cb466e9844467d5181c46458b933df94e0d3061
betaboon <betaboon@0x80.ninja>
2017-04-05 16:41:50 +0000

    refactor nixos-paths.sh

--- nixos-paths.sh
+++ nixos-paths.sh
@@ -1,8 +1,11 @@
 echo "setting up paths..."
 export NIX_DEV_ROOT=$HOME/nixcfg
-export NIX_HOST_CONFIG=$NIX_DEV_ROOT/src/hosts/vps/default.nix
-export NIX_PATH="nixpkgs=$NIX_DEV_ROOT/nixpkgs:nixos=$NIX_DEV_ROOT/nixpkgs/nixos:nixos-config=$NIX_HOST_CONFIG:services=/etc/nixos/services"
+export NIX_PKGS=$NIX_DEV_ROOT/nixpkgs
+export NIX_OS=$NIX_PKGS/nixos
+export NIX_OS_CONFIG=$NIX_DEV_ROOT/src/hosts/vps/default.nix
+export NIX_PATH="nixpkgs=$NIX_PKGS:nixos=$NIX_OS:nixos-config=$NIX_OS_CONFIG:services=/etc/nixos/services"
 
-echo "  NIX_DEV_ROOT=$NIX_DEV_ROOT" 
-echo "  NIX_HOST_CONFIG=$NIX_HOST_CONFIG"
+echo "  NIX_PKGS=$NIX_PKGS"
+echo "  NIX_OS=$NIX_OS"
+echo "  NIX_OS_CONFIG=$NIX_OS_CONFIG"
 echo "  NIX_PATH=$NIX_PATH"
