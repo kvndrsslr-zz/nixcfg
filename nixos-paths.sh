c6df9888e3959ed878829a306742f7e0e3f6e35a
betaboon <betaboon@0x80.ninja>
2017-04-17 21:02:00 +0000

    nixos-paths: cleanup

--- nixos-paths.sh
+++ nixos-paths.sh
@@ -1,11 +1,11 @@
 echo "setting up paths..."
 export NIX_DEV_ROOT=$HOME/nixcfg
-export NIX_PKGS=$NIX_DEV_ROOT/nixpkgs
-export NIX_OS=$NIX_PKGS/nixos
-export NIX_OS_CONFIG=$NIX_DEV_ROOT/src/hosts/vps/default.nix
-export NIX_PATH="nixpkgs=$NIX_PKGS:nixos=$NIX_OS:nixos-config=$NIX_OS_CONFIG:services=/etc/nixos/services"
+export NIXPKGS=$NIX_DEV_ROOT/nixpkgs
+export NIXOS=$NIXPKGS/nixos
+export NIXOS_CONFIG=$NIX_DEV_ROOT/src/hosts/vps/default.nix
+export NIX_PATH="nixpkgs=$NIXPKGS:nixos=$NIXOS:nixos-config=$NIXOS_CONFIG:services=/etc/nixos/services"
 
-echo "  NIX_PKGS=$NIX_PKGS"
-echo "  NIX_OS=$NIX_OS"
-echo "  NIX_OS_CONFIG=$NIX_OS_CONFIG"
+echo "  NIXPKGS=$NIXPKGS"
+echo "  NIXOS=$NIXOS"
+echo "  NIXOS_CONFIG=$NIXOS_CONFIG"
 echo "  NIX_PATH=$NIX_PATH"
